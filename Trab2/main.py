279ef7ae145a67c1420ea50b2d98540e5b4b6889
Angelo Borsoi Ross <suporte.estag2@cmc.pr.gov.br>
2018-08-28 09:06:18 -0300

    Comentarios e formatação

--- Trab2/main.py
+++ Trab2/main.py
@@ -14,18 +14,20 @@ def main(address, port):
 	sockterino.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	sockterino.bind(('', port))
 	
+	# Dar join no grupo multicast
 	sockterino.setsockopt(socket.IPPROTO_IP, 
 	                      socket.IP_ADD_MEMBERSHIP, 
-	                      pack('4sL', socket.inet_aton(address), socket.INADDR_ANY))
+	                      pack('4sL', socket.inet_aton(address), socket.INADDR_ANY))                
 	                      
-	                      
-	                      
+	# Settando Time To Live para 1 para nao sair da rede local
 	sockterino.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, pack('b', 1))
 
+	# Instanciando recursos e threads
 	resources = [Resource(), Resource(8)]
 	threadListener = Listener(sockterino, resources)
 	threadSender = Sender(sockterino, (address, port), resources)
 
+	# Iniciando e aguardando threads
 	threadListener.start()
 	threadSender.start()
 	threadListener.join()
