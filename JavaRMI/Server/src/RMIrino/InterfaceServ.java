2c354c049bc80c30295aecc1a36fa900ab06737b
Angelin01 <angeloborsoiross@hotmail.com>
2018-09-29 19:30:29 -0300

    Atualizados métodos na interface do servidor

--- JavaRMI/Server/src/RMIrino/InterfaceServ.java
+++ JavaRMI/Server/src/RMIrino/InterfaceServ.java
@@ -1,8 +1,19 @@
 package RMIrino;
 
+import Travel.*;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.Date;
+import java.util.List;
 
 public interface InterfaceServ extends Remote {
-     void call(String str, InterfaceCli clientReference) throws RemoteException;
+	List<Lodging> getLodgings() throws RemoteException;
+	List<PlaneTicket> getPlaneTickets() throws RemoteException;
+	List<TravelPackage> getTravelPackages() throws RemoteException;
+
+	boolean buyPlaneTicket(int planeTicketID) throws RemoteException;
+	boolean buyLodging(int lodgingID, int numRooms, Date checkIn, Date checkOut) throws RemoteException;
+	boolean buyLodging(int lodgingID, int numRooms, String checkIn, String checkOut) throws RemoteException;
+	boolean buyTravelPackage(int planeTicketID, int lodgingID, int numRooms, Date checkIn, Date checkOut) throws RemoteException;
+	boolean buyTravelPackage(int planeTicketID, int lodgingID, int numRooms, String checkIn, String checkOut) throws RemoteException;
 }
