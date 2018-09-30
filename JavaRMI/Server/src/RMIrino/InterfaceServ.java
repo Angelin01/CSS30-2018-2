9c681955075d7e37467fa4fe53638093970bf9e2
Angelin01 <angeloborsoiross@hotmail.com>
2018-09-30 18:19:44 -0300

    Métodos para registrar interesse em eventos

--- JavaRMI/Server/src/RMIrino/InterfaceServ.java
+++ JavaRMI/Server/src/RMIrino/InterfaceServ.java
@@ -81,5 +81,35 @@ public interface InterfaceServ extends Remote {
 	 */
 	boolean buyTravelPackage(int travelPackageID, int numRooms, String scheckIn, String scheckOut) throws RemoteException, ParseException;
 
+	/**
+	 * Method for registering interest in new plane tickets
+	 * @param destiny the destiny location for the plane ticket
+	 * @param origin the origin location for the plane ticket
+	 * @param maximumPrice the maximum price for the tickets in CENTS
+	 * @param clientReference the reference on which to notify the client. This interface MUST implement the "notify(int id)" method
+	 * @return the id for the successfully registered event. Returns -1 if there was a failure.
+	 * @throws RemoteException @todo
+	 */
+	int interestPlaneTicket(Location destiny, Location origin, int maximumPrice, InterfaceCli clientReference) throws RemoteException;
 
+	/**
+	 * Method for registering interest in new lodgings
+	 * @param location the location for the lodging
+	 * @param maximumPrice the maximum price for the lodging in CENTS per day per room
+	 * @param clientReference the reference on which to notify the client. This interface MUST implement the "notify(int id)" method
+	 * @return the id for the successfully registered event. Returns -1 if there was a failure.
+	 * @throws RemoteException @todo
+	 */
+	int interestLodging(Location location, int maximumPrice, InterfaceCli clientReference) throws RemoteException;
+
+	/**
+	 * Method for registering interest in new plane tickets
+	 * @param destiny the destiny location for the travel package
+	 * @param origin the origin location for the travel package
+	 * @param maximumPrice the maximum price for the travel package in CENTS
+	 * @param clientReference the reference on which to notify the client. This interface MUST implement the "notify(int id)" method
+	 * @return the id for the successfully registered event. Returns -1 if there was a failure.
+	 * @throws RemoteException @todo
+	 */
+	int interestTravelPackage(Location destiny, Location origin, int maximumPrice, InterfaceCli clientReference) throws RemoteException;
 }
