54e836dd536bbcb78720dd8ac374586de73db098
Angelin01 <angeloborsoiross@hotmail.com>
2018-10-04 22:41:31 -0300

    Começo TravelPackageEvent

---
+++ JavaRMI/Server/src/TravelEvent/TravelPackageEvent.java
@@ -0,0 +1,36 @@
+package TravelEvent;
+
+import RMIrino.InterfaceCli;
+import Travel.Location;
+import Travel.TravelPackage;
+
+import java.util.Date;
+
+public class TravelPackageEvent extends TravelEvent {
+	private Location origin;
+	private Location destiny;
+	private Date departureDate;
+	private Date returnDate;
+
+	/**
+	 * Simple constructor for a TravelPackageEvent notifier
+	 * At least one of the filters must matter
+	 * @param origin the origin Location. null if doesn't matter
+	 * @param destiny the destiny Location. null if doesn't matter
+	 * @param departureDate the Date object for the departure date. null if doesn't matter
+	 * @param returnDate the Date object for the return date. null if doesn't matter
+	 * @param maximumPrice the maximum price for the ticket. <= 0 if doesn't matter
+	 * @param clientReference the client reference, cannot be null
+	 * @throws IllegalArgumentException if none of the filters matter
+	 */
+	public TravelPackageEvent(Location origin, Location destiny, Date departureDate, Date returnDate, int maximumPrice, InterfaceCli clientReference) {
+		super(maximumPrice, clientReference);
+		if (origin == null && destiny == null && departureDate == null && returnDate == null && maximumPrice <= 0) {
+			throw new IllegalArgumentException("At least one filter must matter");
+		}
+		this.origin = origin;
+		this.destiny = destiny;
+		this.departureDate = departureDate;
+		this.returnDate = returnDate;
+	}
+}
