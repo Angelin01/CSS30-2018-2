4a6a41a8c0b7120d5224d4a92341182dc27fb149
Angelin01 <angeloborsoiross@hotmail.com>
2018-10-04 22:05:58 -0300

    Começo de PlaneTicketEvent

--- JavaRMI/Server/src/TravelEvent/PlaneTicketEvent.java
+++ JavaRMI/Server/src/TravelEvent/PlaneTicketEvent.java
@@ -1,4 +1,26 @@
 package TravelEvent;
 
-public class PlaneTicketEvent {
+import RMIrino.InterfaceCli;
+import Travel.Location;
+import Travel.PlaneTicket;
+
+public class PlaneTicketEvent extends TravelEvent {
+	private Location origin;
+	private Location destiny;
+
+	/**
+	 * Simple constructor for a PlaneTicketEvent notifier
+	 * @param origin the origin Location. null if doesn't matter
+	 * @param destiny the destiny Location. null if doesn't matter
+	 * @param maximumPrice the maximum price for the ticket. <= 0 if doesn't matter
+	 * @param clientReference the client reference
+	 */
+	public PlaneTicketEvent(Location origin, Location destiny, int maximumPrice, InterfaceCli clientReference) {
+		super(maximumPrice, clientReference);
+		if (origin == null && destiny == null && maximumPrice <= 0) {
+			throw new IllegalArgumentException("At least one filter must matter");
+		}
+		this.origin = origin;
+		this.destiny = destiny;
+	}
 }
