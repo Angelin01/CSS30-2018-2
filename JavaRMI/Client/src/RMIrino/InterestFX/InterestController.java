ecdb432ece618d402cb0756ca6448fb9400ab66f
khellwan <thiagobispinho@hotmail.com>
2018-10-14 21:57:48 -0300

    Fixed interest registering bug

--- JavaRMI/Client/src/RMIrino/InterestFX/InterestController.java
+++ JavaRMI/Client/src/RMIrino/InterestFX/InterestController.java
@@ -86,7 +86,7 @@ public class InterestController {
         this.server = server;
         this.client = client;
         this.masterData = masterData;
-        //registryTable.setItems(masterData);
+        registryTable.setItems(masterData);
     }
 
     /**
@@ -102,6 +102,7 @@ public class InterestController {
         Date departure;
         Date returnn = null;
         int price = 0;
+        boolean success = false;
 
 
         destiny = (destinyField == null) ? null : destinyField.getSelectionModel().getSelectedItem();
@@ -110,22 +111,27 @@ public class InterestController {
         returnn = (returnField.getValue() == null) ? null : Date.from(returnField.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant());
         price = (priceField.getText().isEmpty()) ? 0 : Integer.valueOf(priceField.getText());
 
-        if (optionChoiceBox.getSelectionModel().getSelectedItem() == "Passagem"){
+        if (origin == null || departure == null || optionChoiceBox.getSelectionModel().getSelectedItem().isEmpty()){
+            success = false;
+        }
+        else if (optionChoiceBox.getSelectionModel().getSelectedItem() == "Passagem"){
             // Location destiny, Location origin, Date departureDate, Date returnDate, int maximumPrice, InterfaceCli clientReference
             registry = server.interestPlaneTicket(destiny, origin, departure, returnn, price, client);
             reg = new Registry(registry, price, "Passagem", destiny , origin, departure, returnn);
+            success = true;
 
         } else if (optionChoiceBox.getSelectionModel().getSelectedItem() == "Hospedagem"){
             // Location location, Date checkIn, Date checkOut, int maximumPrice, InterfaceCli clientReference
             registry = server.interestLodging(destiny, departure, returnn, price, client);
             reg = new Registry(registry, price, "Hospedagem",destiny, origin, departure, returnn);
+            success = true;
         } else {
             // Location destiny, Location origin, Date departureDate, Date returnDate, int maximumPrice, InterfaceCli clientReference
             registry = server.interestTravelPackage(destiny, origin, departure, returnn, price, client);
             reg = new Registry(registry, price, "Pacote", destiny, origin, departure, returnn);
-
+            success = true;
         }
-        if (registry != -1){
+        if (registry != -1 && success == true){
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("Informação de registro");
             alert.setContentText("Registro criado com sucesso!");
@@ -136,7 +142,7 @@ public class InterestController {
         } else {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Informação da registro");
-            alert.setContentText("Erro na criação do  registro.");
+            alert.setContentText("Erro na criação do registro.");
             alert.setHeaderText(null);
             alert.showAndWait();
         }
@@ -150,7 +156,6 @@ public class InterestController {
         Registry reg = registryTable.getSelectionModel().getSelectedItem();
         Boolean removed = false;
         if (reg.getType() == "Passagem") {
-            System.out.println(server.getLodgings());
             removed = server.removeInterestPlaneTicket(reg.getId(), reg.getDestiny(), reg.getDepartureDate());
         } else if (reg.getType() == "Hospedagem") {
             removed = server.removeInterestLodging(reg.getId(), reg.getDestiny(), reg.getDepartureDate());
