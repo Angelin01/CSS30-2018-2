a670866d7fdd3f8b171e6dd0c12503481e3d80e2
Angelin01 <angeloborsoiross@hotmail.com>
2018-11-25 21:51:57 -0200

    Adicionado logger

--- Trab5/ServLodgings/src/RMIrino/Main.java
+++ Trab5/ServLodgings/src/RMIrino/Main.java
@@ -1,12 +1,23 @@
 package RMIrino;
 
+import java.io.IOException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class Main {
-	public static void main(String[] args) throws RemoteException {
+	public static void main(String[] args) throws IOException {
 		final int PORT = 1339;
+		Logger logger = Logger.getLogger("LodgingLog");
+		FileHandler logHandler = new FileHandler("./lodging.log");
+		logger.addHandler(logHandler);
+		logHandler.setFormatter(new SimpleFormatter());
+
+
+		logger.info("Starting up Lodging system");
 		Registry referenciaServicoNomes = LocateRegistry.createRegistry(PORT);
 		// Implementar serviço
 	}
--- Trab5/ServPlaneTickets/src/RMIrino/Main.java
+++ Trab5/ServPlaneTickets/src/RMIrino/Main.java
@@ -1,12 +1,24 @@
 package RMIrino;
 
+import java.io.IOException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class Main {
-	public static void main (String[] args) throws RemoteException {
+	public static void main (String[] args) throws IOException {
 		final int PORT = 1338;
+		Logger logger = Logger.getLogger("PlaneTicketLog");
+		FileHandler logHandler = new FileHandler("./plane-ticket.log");
+		logger.addHandler(logHandler);
+		logHandler.setFormatter(new SimpleFormatter());
+
+
+		logger.info("Starting up Plane Ticket system");
+
 		Registry referenciaServicoNomes = LocateRegistry.createRegistry(PORT);
 		// Implementar serviço
 	}
