16d91b4f6762602c8ec9b5b4c41c235d5e33b4df
Angelin01 <angeloborsoiross@hotmail.com>
2018-11-24 22:47:00 -0200

    Estrutura básica para servidor de PlaneTickets

---
+++ Trab5/ServPlaneTickets/src/RMIrino/InterfacePlaneTicket.java
@@ -0,0 +1,15 @@
+package RMIrino;
+
+import Travel.Location;
+import Travel.Lodging;
+import Travel.PlaneTicket;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.Date;
+import java.util.List;
+
+public interface InterfacePlaneTicket extends Remote {
+	
+}
+
