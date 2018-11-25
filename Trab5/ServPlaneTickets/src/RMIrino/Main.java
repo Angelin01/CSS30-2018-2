a8de1d89962127fc38e9958f86f88aacb933d99d
Angelin01 <angeloborsoiross@hotmail.com>
2018-11-24 22:58:28 -0200

    Main básico para servers

--- Trab5/ServPlaneTickets/src/RMIrino/Main.java
+++ Trab5/ServPlaneTickets/src/RMIrino/Main.java
@@ -1,7 +1,13 @@
 package RMIrino;
 
-public class Main {
-	public static void main (String[] args) {
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
+public class Main {
+	public static void main (String[] args) throws RemoteException {
+		final int PORT = 1338;
+		Registry referenciaServicoNomes = LocateRegistry.createRegistry(PORT);
+		// Implementar serviço
 	}
 }
