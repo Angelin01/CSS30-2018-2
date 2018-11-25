5af8b1e7f31398dd0bbdf2393f0a8b889f78b864
Angelin01 <angeloborsoiross@hotmail.com>
2018-11-24 22:47:16 -0200

    Interface de PlaneTickets

--- Trab5/ServPlaneTickets/src/RMIrino/InterfacePlaneTicket.java
+++ Trab5/ServPlaneTickets/src/RMIrino/InterfacePlaneTicket.java
@@ -10,6 +10,35 @@ import java.util.Date;
 import java.util.List;
 
 public interface InterfacePlaneTicket extends Remote {
-	
+	/**
+	 * Simple getter for the list of available plane tickets
+	 * @return a List of ALL PlaneTickets objects
+	 * @throws RemoteException if there's any problem with the remote connection
+	 */
+	List<PlaneTicket> getPlaneTickets() throws RemoteException;
+
+	/**
+	 * Getter for PlaneTickets with filters
+	 * @param origin the origin location. null if doesn't matter
+	 * @param destiny the destiny location. null if doesn't matter
+	 * @param maxPrice the maximum price. &lt;= 0 if doesn't matter
+	 * @param departureDate the departure date. null if doesn't matter
+	 * @param returnDate the return date. null if doesn't matter. Note: having a return date implies a round-trip
+	 * @param minimumSeats the minimum number of available seats. &lt;= 0 if doesn't matter
+	 * @return A list of PlaneTickets according to the filters
+	 * @throws RemoteException if there's any problem with the remote connection
+	 */
+	List<PlaneTicket> getPlaneTickets(Location origin, Location destiny, int maxPrice, Date departureDate,
+	                                  Date returnDate, int minimumSeats) throws RemoteException;
+
+
+	/**
+	 * Method for buying a Travel Package using Date objects
+	 * @param travelPackageID the id for the desired travel package
+	 * @param numPackets the number of rooms desired for the lodging
+	 * @return true if the travel package was successfully bought, false if there was a problem
+	 * @throws RemoteException if there's any problem with the remote connection
+	 */
+	boolean buyTravelPackage(int travelPackageID, int numPackets) throws RemoteException;
 }
 
