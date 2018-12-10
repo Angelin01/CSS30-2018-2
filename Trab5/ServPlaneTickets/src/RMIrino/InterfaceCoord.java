cdb602563099bcd345ac145375fa05e8adb9b448
Angelin01 <angeloborsoiross@hotmail.com>
2018-12-10 21:56:24 -0200

    Interface do coordenador

---
+++ Trab5/ServPlaneTickets/src/RMIrino/InterfaceCoord.java
@@ -0,0 +1,14 @@
+package RMIrino;
+
+import java.io.IOException;
+import java.rmi.Remote;
+
+public interface InterfaceCoord extends Remote {
+
+	/**
+	 *
+	 * @param idTransaction the id for the current transaction
+	 * @return true if the transaction should continue or false if it should be aborted
+	 */
+	Boolean continueTransaction(int idTransaction) throws IOException;
+}
